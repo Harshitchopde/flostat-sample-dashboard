51a50ab20bbc050826672db98b89a59f82d1f870
Harshitchopde <chopdeharshit@gmail.com>
2025-07-31 17:37:01 +0530

    till now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import ControlPanel from './components/ControlPanel';
+import PumpStatusMonitor from './components/PumpStatusMonitor';
+
 
 
 function App() {
   return (
     <div className="App flex w-full h-[100vh] items-center justify-center border border-blue-500">
-      <ControlPanel/>
+      <PumpStatusMonitor/>
     </div>
   );
 }
---
+++ src/constants/constant.js
@@ -0,0 +1,6 @@
+export const YOUR_COGNITO_IDENTITY_POOL_ID ="ap-south-1:fb6c198c-5717-40fa-a52a-4de3759a9038"
+// export const YOUR_COGNITO_IDENTITY_POOL_ID ="ap-south-1:f4463981-a195-4bec-b919-2e77c7383363"
+export const YOUR_AWS_REGION = "ap-south-1"
+export const YOUR_IOT_ENDPOINT = "a3a6bcyydw1uzn-ats.iot.ap-south-1.amazonaws.com"
+// export const YOUR_IOT_ENDPOINT = "wss://a3a6bcyydw1uzn-ats.iot.ap-south-1.amazonaws.com/mqtt";
+// a3a6bcyydw1uzn-ats.iot.ap-south-1.amazonaws.com
